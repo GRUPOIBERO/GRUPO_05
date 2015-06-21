f594fb33378feb89800afb5f7956c34d4bba822b
Kely Vanessa Ayma Taipe <a1410151@iberotec.edu.pe>
2015-06-21 00:13:36 -1200

    REGISTRAR CLIENTE

--- src/Sistema/Registro_Clientes.java
+++ src/Sistema/Registro_Clientes.java
@@ -114,6 +114,11 @@ public class Registro_Clientes extends javax.swing.JFrame {
         jInternalFrame4.setVisible(true);
 
         jButton4.setText("Agregar Personas");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jButton5.setText("Eliminar Personas");
 
@@ -250,11 +255,11 @@ public class Registro_Clientes extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
-        // TODO add your handling code here:
+     
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+       
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
@@ -268,6 +273,10 @@ public class Registro_Clientes extends javax.swing.JFrame {
         res.setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton10ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
