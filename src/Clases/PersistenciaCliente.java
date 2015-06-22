f52ed6691c2b710aa9a8b5309a23fe7b270a32ec
raul1995 <tamani1995@gmail.com>
2015-06-22 16:31:02 -0500

    raul1995 <tamani1995@gmail.com>

--- src/Clases/PersistenciaCliente.java
+++ src/Clases/PersistenciaCliente.java
@@ -1,12 +1,10 @@
  package Clases;
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-
 /**
  *
  * @author RAUL
