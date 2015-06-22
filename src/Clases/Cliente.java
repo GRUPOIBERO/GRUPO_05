5c8c2627718b87131dd953139474d3b6c2ff94f2
GRUPOIBERO <a1410358@iberotec.edu.pe>
2015-06-22 16:58:53 -0500

    RAUL TAMANI ARBILDO

--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -13,58 +13,34 @@ public class Cliente {
     public Cliente() {
     }
 
-    /**
-     * @return the nombre
-     */
     public ArrayList<String> getNombre() {
         return nombre;
     }
 
-    /**
-     * @param nombre the nombre to set
-     */
     public void setNombre(ArrayList<String> nombre) {
         this.nombre = nombre;
     }
 
-    /**
-     * @return the apellido
-     */
     public ArrayList<String> getApellido() {
         return apellido;
     }
 
-    /**
-     * @param apellido the apellido to set
-     */
     public void setApellido(ArrayList<String> apellido) {
         this.apellido = apellido;
     }
 
-    /**
-     * @return the edad
-     */
     public ArrayList<Integer> getEdad() {
         return edad;
     }
 
-    /**
-     * @param edad the edad to set
-     */
     public void setEdad(ArrayList<Integer> edad) {
         this.edad = edad;
     }
 
-    /**
-     * @return the direccion
-     */
     public ArrayList<String> getDireccion() {
         return direccion;
     }
 
-    /**
-     * @param direccion the direccion to set
-     */
     public void setDireccion(ArrayList<String> direccion) {
         this.direccion = direccion;
     }
